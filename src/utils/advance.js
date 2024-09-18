da3b6b13fd94ec1ce9b3cc6cb7bb853d91fde95d
wo-o29 <woogur29@gmail.com>
2024-09-18 21:06:47 +0900

    refactor: 상수 변경

--- src/utils/advance.js
+++ src/utils/advance.js
@@ -1,11 +1,11 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
-import { MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER } from "../constants/index.js";
+import { MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER, ADVANCE_NUMBER } from "../constants/index.js";
 
 export const advanceCars = (carsInfo) => {
   const keys = Object.keys(carsInfo);
   keys.forEach((key) => {
     const randomNumber = MissionUtils.Random.pickNumberInRange(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER);
-    if (randomNumber >= 4) {
+    if (randomNumber >= ADVANCE_NUMBER) {
       carsInfo[key] += 1;
     }
   });

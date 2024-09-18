f78cf8990217555fe1a14bc7cf21a67b55541223
wo-o29 <woogur29@gmail.com>
2024-09-18 20:24:15 +0900

    feat: 상수 추가

---
+++ src/constants/index.js
@@ -0,0 +1,11 @@
+// message
+const FIVE_LENGTH_OVER_MESSAGE = "이름은 5자 이하만 가능합니다.";
+const START_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+const COUNT_MESSAGE = "시도할 횟수는 몇 회인가요?";
+const WINNER_MESSAGE = "최종 우승자 : ";
+
+// number
+const MAX_NAME_LENGTH = 5;
+const ADVANCE_NUMBER = 4;
+const MIN_RANDOM_NUMBER = 1;
+const MAX_RANDOM_NUMBER = 9;

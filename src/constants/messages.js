3889953c3e7bc70be295b588c1efd8948f98d715
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2024-09-26 02:02:09 +0900

    feat: messages 상수

---
+++ src/constants/messages.js
@@ -0,0 +1,27 @@
+// 기본 출력 메시지
+export const MESSAGES = {
+  START: "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)",
+  ANSWER_COUNT: "시도할 횟수는 몇 회인가요?",
+  RESULT: "실행 결과",
+  WINNER: "최종 우승자 : ",
+};
+
+// 규칙
+export const RULES = {
+  CAR_MAX_LENGTH: 5,
+};
+
+//유효성 검사 메시지
+export const ERROR_MESSAGES = {
+  CAR_ERROR_MESSAGES: {
+    TOO_LONG: "[ERROR] 자동차 이름이 너무 깁니다. 5자 이하로 입력해주세요.",
+    BLANK: "[ERROR] 입력하지 않았습니다.",
+    DUPLICAT: "[ERROR] 자동차 이름이 중복됩니다. 중복되지 않게 입력해주세요.",
+    NO_NAME:
+      "[ERROR] 중간에 이름이 없는 자동차가 있습니다. 이름을 모두 입력해주세요.",
+  },
+  COUNT_ERROR_MESSAGES: {
+    ONLY_NUMBER: "[ERROR] 숫자만 입력 가능합니다.",
+    NOT_NEGATIVE: "[ERROR] 1이상의 수만 입력 가능합니다.",
+  },
+};

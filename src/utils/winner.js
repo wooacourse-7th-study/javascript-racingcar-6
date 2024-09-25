3c8fc25bdc49c4e6934e59c7036e0b2a313734f4
wo-o29 <woogur29@gmail.com>
2024-09-25 23:08:07 +0900

    refactor: 최대 거리 구하는 로직 함수로 분리

--- src/utils/winner.js
+++ src/utils/winner.js
@@ -1,7 +1,21 @@
+const getMaxDistance = (carsInfoEntries) => {
+  let maxDistance = 0;
+
+  carsInfoEntries.forEach(([_, distance]) => {
+    if (distance > maxDistance) {
+      maxDistance = distance;
+    }
+  });
+
+  return maxDistance;
+};
+
 export const getWinners = (getCarsInfo) => {
   const carsInfoEntries = Object.entries(getCarsInfo());
-  const maxDistance = Math.max(...carsInfoEntries.map(([_, distance]) => distance));
-  const winners = carsInfoEntries.filter(([_, distance]) => distance === maxDistance).map(([key]) => key.split("-")[1]);
+  const maxDistance = getMaxDistance(carsInfoEntries);
+  const winners = carsInfoEntries
+    .filter(([_, distance]) => distance === maxDistance)
+    .map(([carNames]) => carNames.split("-")[1]);
 
   return winners.join(", ");
 };

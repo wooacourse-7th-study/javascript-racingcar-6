91f8d384537c7a93612b5885a17d300983cba9b8
jiyeeeah <sije3757@snu.ac.kr>
2024-09-26 17:49:50 +0900

    [feat] 경주할 자동차의 이름 입력 받기
    - 게임 시작시, 경주할 자동차의 이름을 입력 받음
    - 자동차의 이름은 쉼표로 구분하여 playersMap에 저장
    - playersMap을 map객체로 하여 추후 전진 상황을 저장할 수 있도록 함
    - 따라서 모든 player들은 value로 0을 갖고 시작함

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+import { MESSAGES } from "./constants/message.js";
+
 class App {
-  async play() {}
+  async play() {
+    // 게임 시작
+    const playerInput = await MissionUtils.Console.readLineAsync(MESSAGES.START_INPUT);
+    const playersMap = new Map(playerInput.split(",").map((player) => [player, 0]));
+  }
 }
 
 export default App;

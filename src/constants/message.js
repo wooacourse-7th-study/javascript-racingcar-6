70bf4cbaf679dae6e37a1c1ac29568142823600a
jiyeeeah <sije3757@snu.ac.kr>
2024-09-26 17:48:45 +0900

    [feat] 게임에 필요한 문구들 상수화
    - 게임 진행에 필요한 입력, 출력 문구들 상수화하여 constants/message.js에 저장

---
+++ src/constants/message.js
@@ -0,0 +1,7 @@
+// 게임 기본 출력 메시지
+export const MESSAGES = {
+  START_INPUT: "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분) \n",
+  TRY_INPUT: "시도할 횟수는 몇 회인가요? \n",
+  RESULT: "실행 결과",
+  WINNER: "최종 우승자 : ",
+};

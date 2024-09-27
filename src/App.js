bbecf90e929301ce1e5e592ff718141fc741d973
DevTeddy123 <ab315601@naver.com>
2024-09-27 22:13:56 +0900

    [feat] App.js에 utils함수 추가 및 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import { MissionUtils } from '@woowacourse/mission-utils'
+import { MESSAGE } from './constants/message.js'
+import {
+	inputCarsNameAndValidate,
+	inputTryCountAndValidate,
+	racingResult,
+	winner,
+} from './utils/index.js'
+
 class App {
-	async play() {}
+	async play() {
+		//자동차 이름 입력
+		MissionUtils.Console.print(MESSAGE.START)
+		const carsNameSplit = await inputCarsNameAndValidate()
+
+		//시도할 횟수 입력
+		MissionUtils.Console.print(MESSAGE.TRY_COUNT)
+		const tryCount = await inputTryCountAndValidate()
+
+		//레이싱 결과 출력
+		MissionUtils.Console.print(MESSAGE.RESULT)
+		const resultArr = racingResult(carsNameSplit, tryCount)
+		const printWinner = winner(resultArr)
+		MissionUtils.Console.print(`${MESSAGE.END}${printWinner}`)
+	}
 }
 
 const app = new App()

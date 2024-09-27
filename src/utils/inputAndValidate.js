eb97b6197aaa576f2eb905e1d98b3c9c8db82c93
DevTeddy123 <ab315601@naver.com>
2024-09-27 22:10:54 +0900

    [feat] 카트 이름 리스트, 시도 횟수 입력 (print 및 validate)

---
+++ src/utils/inputAndValidate.js
@@ -0,0 +1,48 @@
+import { MissionUtils } from '@woowacourse/mission-utils'
+
+export const inputCarsNameAndValidate = async () => {
+	const userInputCarName = await MissionUtils.Console.readLineAsync('')
+
+	const carsSplit = userInputCarName.split(',')
+
+	// 입력이 빈값을 넣을때
+	carsSplit.map(car => {
+		if (car.trim().length === 0) {
+			throw new Error('[ERROR]')
+		}
+	})
+
+	// 자동차 이름 5글자 이상
+	carsSplit.map(el => {
+		if (el.length > 5) {
+			throw new Error('[ERROR]')
+		}
+	})
+
+	// 자동차 이름 중복 확인
+	const set = new Set(carsSplit)
+	if (carsSplit.length !== set.size) {
+		throw new Error('[ERROR]')
+	}
+
+	return carsSplit
+}
+
+export const inputTryCountAndValidate = async () => {
+	const userInputTryCount = await MissionUtils.Console.readLineAsync('')
+	//빈값이 들어오는지
+	if (userInputTryCount.trim() === '') {
+		throw new Error('[ERROR]')
+	}
+
+	//숫자인지
+	if (isNaN(userInputTryCount)) {
+		throw new Error('[ERROR]')
+	}
+
+	//시도횟수 0이상 입력 가능
+	if (userInputTryCount <= 0) {
+		throw new Error('[ERROR]')
+	}
+	return userInputTryCount
+}

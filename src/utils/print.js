c5298dcf30ff30d6f7b81eea452cc9ee8774a8a6
wo-o29 <woogur29@gmail.com>
2024-09-30 17:07:18 +0900

    refactor: 프린트 함수 개행 조건 제거

--- src/utils/print.js
+++ src/utils/print.js
@@ -2,17 +2,12 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 
 export const resultPrint = (getCarsInfo) => {
   const entries = Object.entries(getCarsInfo());
-  const result = entries.map(([carName, advanceCount], idx) => {
+  const result = entries.map(([carName, advanceCount]) => {
     const [_, name] = carName.split("-");
     const dash = "-".repeat(advanceCount);
-
-    // 마지막이라면 개행 추가
-    if (idx === entries.length - 1) {
-      return `${name} : ${dash}\n`;
-    }
 
     return `${name} : ${dash}`;
   });
 
-  MissionUtils.Console.print(result.join("\n"));
+  MissionUtils.Console.print(result.join("\n") + "\n");
 };

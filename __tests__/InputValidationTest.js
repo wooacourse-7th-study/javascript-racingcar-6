6ab7ec79c0469d633667520b537d41fda0b8e746
jiyeeeah <sije3757@snu.ac.kr>
2024-10-01 13:42:54 +0900

    [feat] 추가된 유효성 검사 관련 test 추가
    - 입력값 관련하여 추가된 유효성 검사에 대하여
      InputValidationTest.js에 test를 추가함
    - 시도 횟수 입력값이 1 미만인 경우는 node src/index.js로 실행시,
      정상적으로 1 미만의 값은 입력할 수 없다는 메세지가 나오지만
      test 코드를 실행하면 숫자만 입력가능하다는 메세지가 나옴 => test를 통과 못함
      - 이 부분은 원인 파악 후 수정 예정. 현재 주석처리해둠

--- __tests__/InputValidationTest.js
+++ __tests__/InputValidationTest.js
@@ -12,14 +12,21 @@ const mockQuestions = (inputs) => {
 };
 
 describe("입력값 유효성 검사", () => {
-  const CAR_ERROR_CASE = [[["car1,carcar2"], ERROR_MESSAGES.LIMIT]];
+  const CAR_ERROR_CASE = [
+    [["car1,carcar2"], ERROR_MESSAGES.LIMIT],
+    [["car, "], ERROR_MESSAGES.NO_BLANK],
+  ];
   test.each(CAR_ERROR_CASE)("자동차 이름에 대한 예외 처리", async (inputs, message) => {
     mockQuestions(inputs);
     const app = new App();
     await expect(app.play()).rejects.toThrow(message);
   });
 
-  const TRIAL_ERROR_CASE = [[["hi"], ERROR_MESSAGES.NOT_NUMBER]];
+  const TRIAL_ERROR_CASE = [
+    [["hi"], ERROR_MESSAGES.NOT_NUMBER],
+    [[" "], ERROR_MESSAGES.NO_BLANK],
+    // [["0"], ERROR_MESSAGES.NO_UNDER_1],
+  ];
   test.each(TRIAL_ERROR_CASE)("시도 횟수에 대한 예외 처리", async (inputs, message) => {
     mockQuestions(inputs);
     const app = new App();

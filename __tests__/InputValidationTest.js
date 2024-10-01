57fdd07f96d52e280c2d953575e74a88b848e7ae
jiyeeeah <sije3757@snu.ac.kr>
2024-10-01 15:53:53 +0900

    [bugfix] test코드에서 NO_UNDER_1 에러메시지 안뜨던 오류 수정
    - mockQuestions에서 shift()메서드를 제거하고,
      ERROR_CASE 배열의 input 부분을 배열이 아닌 문자열 형태로 수정함

--- __tests__/InputValidationTest.js
+++ __tests__/InputValidationTest.js
@@ -2,19 +2,18 @@ import App from "../src/App.js";
 import { MissionUtils } from "@woowacourse/mission-utils";
 import { ERROR_MESSAGES } from "../src/constants/message.js";
 
-const mockQuestions = (inputs) => {
+const mockQuestions = (input) => {
   MissionUtils.Console.readLineAsync = jest.fn();
 
   MissionUtils.Console.readLineAsync.mockImplementation(() => {
-    const input = inputs.shift();
     return Promise.resolve(input);
   });
 };
 
 describe("입력값 유효성 검사", () => {
   const CAR_ERROR_CASE = [
-    [["car1,carcar2"], ERROR_MESSAGES.LIMIT],
-    [["car, "], ERROR_MESSAGES.NO_BLANK],
+    ["car1,carcar2", ERROR_MESSAGES.LIMIT],
+    ["car, ", ERROR_MESSAGES.NO_BLANK],
   ];
   test.each(CAR_ERROR_CASE)("자동차 이름에 대한 예외 처리", async (inputs, message) => {
     mockQuestions(inputs);
@@ -23,9 +22,9 @@ describe("입력값 유효성 검사", () => {
   });
 
   const TRIAL_ERROR_CASE = [
-    [["hi"], ERROR_MESSAGES.NOT_NUMBER],
-    [[" "], ERROR_MESSAGES.NO_BLANK],
-    // [["0"], ERROR_MESSAGES.NO_UNDER_1],
+    ["hi", ERROR_MESSAGES.NOT_NUMBER],
+    [" ", ERROR_MESSAGES.NO_BLANK],
+    ["0", ERROR_MESSAGES.NO_UNDER_1],
   ];
   test.each(TRIAL_ERROR_CASE)("시도 횟수에 대한 예외 처리", async (inputs, message) => {
     mockQuestions(inputs);
